8f211fdfa990a85d278b3e9dc7a384be4c350631
qussa <qussa@126.com>
2018-07-13 12:51:50 +0800

    #6

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# nodejs-mongo-wx" 
+
+git add *
+git add <filename>
+git commit -m "代码提交信息"
+git push origin master
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const options = {};
 options.ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
  options.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 // options.config = { name: 'Total.js' };
-// options.sleep = 3000;
+options.sleep = 3000;
 
 require('total.js').http('release', options);
 // require('total.js').cluster.http(5, 'release', options);
