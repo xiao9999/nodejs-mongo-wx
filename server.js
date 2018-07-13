d90fa4b5330430c80da99d9814bfcea8827ffcb1
qussa <qussa@126.com>
2018-07-13 11:28:34 +0800

    代码提交信息

---
+++ server.js
@@ -0,0 +1,15 @@
+// ===================================================
+// FOR PRODUCTION
+// Total.js - framework for Node.js platform
+// https://www.totaljs.com
+// ===================================================
+
+const options = {};
+
+options.ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+ options.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+
+require('total.js').http('release', options);
+// require('total.js').cluster.http(5, 'release', options);
